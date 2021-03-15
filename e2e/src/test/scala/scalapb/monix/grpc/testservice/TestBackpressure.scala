63dac6ce685110aab33e9c99b05b176fc4895879
Boris Smidt <boris.smidt@restore.eu>
2021-03-15 11:47:05 +0100

    Fix tests

--- e2e/src/test/scala/scalapb/monix/grpc/testservice/TestBackpressure.scala
+++ e2e/src/test/scala/scalapb/monix/grpc/testservice/TestBackpressure.scala
@@ -4,11 +4,12 @@ package scalapb.monix.grpc.testservice
  * Copyright (C) 15.03.21 - REstore NV
  */
 
-class TestBackpressure extends GrpcBaseSpec(8002) {
+class TestBackpressure extends munit.FunSuite with GrpcServerFixture {
+  val stub = clientFixture(8002)
+  override def munitFixtures = List(stub)
 
   test("bidi stream calls should backpressure on client side") {
     val client = stub()
-    client.serverStreaming()
   }
 
   test("bidi stream calls should backpressure on server side") {
